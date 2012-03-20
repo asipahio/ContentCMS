931b8990297c6ea2f6c58d1f3bde28e99137c81c
Ali Sipahioglu <asipahio@gmail.com>
2012-03-20 13:40:07 -0400

    Initial commit

---
+++ ContentCMS/Controllers/PagesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContentCMS.Models;
+
+namespace ContentCMS.Controllers
+{
+    public class PagesController : Controller
+    {
+        contentCMSDBDataContext db = new contentCMSDBDataContext();
+
+        public ActionResult Index(string pageNames, string section)
+        {
+            string pagePath = "/" + section;
+
+            if (!string.IsNullOrEmpty(pageNames))
+            {
+                foreach (string pageName in pageNames.Split('/'))
+                {
+                    if (pageName.Length > 0)
+                    {
+                        pagePath += "/" + pageName;
+                    }
+                }
+            }
+
+            pagePath += "/";
+
+            var page = (from p in db.pages where p.pageURL.Equals(pagePath) select p).FirstOrDefault();
+
+            if (page != null)
+            {
+                ViewData["page_title"] = page.pageTitle;
+                ViewData["page_text"] = page.pageContent;
+            }
+
+            ViewData["pageURL"] = pagePath;
+
+            Pages pages = new Pages("/" + section + "/");
+            var navItems = pages.getPages();
+
+            return View(navItems);
+        }
+
+        public PartialViewResult Navigation()
+        {
+            var rd = ControllerContext.ParentActionViewContext.RouteData;
+            string currentController = rd.GetRequiredString("controller");
+
+            Pages pages = new Pages(currentController);
+
+            var navItems = pages.getNavigation();
+
+            return PartialView(navItems);
+        }
+
+        public PartialViewResult Sidebar(string id)
+        {
+            string currentController = id;
+            if (string.IsNullOrEmpty(id))
+            {
+                var rd = ControllerContext.ParentActionViewContext.RouteData;
+                currentController = rd.GetRequiredString("controller");
+            }
+            Pages pages = new Pages("/" + currentController + "/");
+
+            var navItems = pages.getPages();
+
+            ViewData["pageURL"] = currentController;
+
+            return PartialView(navItems);
+        }
+
+    }
+}
---
+++ ContentCMS/Models/Pages.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContentCMS.Models
+{
+    public class Pages
+    {
+        private string controllerName;
+        contentCMSDBDataContext db = new contentCMSDBDataContext();
+
+        public Pages(string controllerName)
+        {
+            this.controllerName = controllerName;
+        }
+
+        public IQueryable<pageLinks> getPages()
+        {
+            int parentID;
+            var parentPage = (from pp in db.pages
+                              where pp.pageURL.Equals(this.controllerName) && pp.isPublished == true
+                              select pp).FirstOrDefault();
+            if (parentPage == null) { parentID = -1; }
+            else
+            {
+                parentID = parentPage.pageID;
+            }
+            var pages = from p in db.pages
+                        where p.parent.Equals(parentID) && p.isPublished == true
+                        orderby p.pageOrder
+                        select new pageLinks
+                        {
+                            pageName = p.pageTitle,
+                            pageURL = p.isStatic ? p.pageURL : "/p" + p.pageURL
+                        };
+
+            return pages;
+        }
+
+        public IQueryable<navigation> getNavigation()
+        {
+            var pages = from p in db.pages
+                        where p.parent.Equals(0) && p.isPublished.Equals(true)
+                        orderby p.pageOrder
+                        select new navigation
+                        {
+                            pageName = p.pageTitle,
+                            pageURL = p.isContent ? (p.isStatic ? p.pageURL : "/p" + p.pageURL) : null,
+                            subPages = (from sp in db.pages
+                                        where sp.parent.Equals(p.pageID) && sp.isPublished.Equals(true)
+                                        orderby p.pageOrder
+                                        select new pageLinks
+                                        {
+                                            pageName = sp.pageTitle,
+                                            pageURL = sp.isStatic ? sp.pageURL : "/p" + sp.pageURL
+                                        })
+                        };
+
+            return pages;
+        }
+
+        public class navigation
+        {
+            public string pageName { get; set; }
+            public IEnumerable<pageLinks> subPages { get; set; }
+            public string pageURL { get; set; }
+        }
+
+        public class pageLinks
+        {
+            public string pageName { get; set; }
+            public string pageURL { get; set; }
+
+        }
+    }
+}
